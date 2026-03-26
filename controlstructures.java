6ec65cd495fbbda4954a36875fc09c981b9d51c4
thanush <thanushrohan09@gmail.com>
2026-03-25 20:43:53 -0800

    updated constrol structures

---
+++ controlstructures.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class controlstructures {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(x: "Enter the age");
+        int age = sc.nextInt();
+        if(age >=18)
+        System.out.println(x: "Eligible for voting");
+        sc.close();
+
+        if.else biggest of two numbers
+        scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        if (a > b)
+        System.out.println(x:"First number: ");
+        else
+        System.out.println("second number:  ");
+        sc.close();
+
+
+    }
+}
